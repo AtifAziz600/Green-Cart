94e7da3d193fbf2068fd086ac2be3731fceea761
AtifAziz600 <feralligatrfahim630@gmail.com>
2025-05-15 03:37:05 +0600

    r

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -6,6 +6,9 @@ const MainBanner = () => {
     <div className='relative'>
       <img src={assets.main_banner_bg} alt='banner' className='w-full hidden md:block'/>
       <img src={assets.main_banner_bg_sm} alt="banner_sm" className='w-full md:hidden' />
+      <div>
+        <h1>Freshness you can trust, Savings you will love</h1>
+      </div>
     </div>
   )
 }
