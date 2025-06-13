b67931cd7104fd3a6b95b1593156b997c0b405b0
AtifAziz600 <feralligatrfahim630@gmail.com>
2025-06-13 19:43:19 +0600

    t

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -22,19 +22,12 @@ const ProductCard = ({ product }) => {
             .fill("")
             .map((_, i) =>
               product.rating > i ? (
-                <svg
+                <img
                   key={i}
-                  width="14"
-                  height="13"
-                  viewBox="0 0 18 17"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8.049.927c.3-.921 1.603-.921 1.902 0l1.294 3.983a1 1 0 0 0 .951.69h4.188c.969 0 1.371 1.24.588 1.81l-3.388 2.46a1 1 0 0 0-.364 1.118l1.295 3.983c.299.921-.756 1.688-1.54 1.118L9.589 13.63a1 1 0 0 0-1.176 0l-3.389 2.46c-.783.57-1.838-.197-1.539-1.118L4.78 10.99a1 1 0 0 0-.363-1.118L1.028 7.41c-.783-.57-.38-1.81.588-1.81h4.188a1 1 0 0 0 .95-.69z"
-                    fill="#615fff"
-                  />
-                </svg>
+                  className="md:w-3.5 w-3"
+                  src={i < 4 ? assets.star_icon : assets.star_dull_icon}
+                  alt=""
+                />
               ) : (
                 <svg
                   width="14"
