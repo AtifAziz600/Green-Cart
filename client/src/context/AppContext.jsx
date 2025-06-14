eac020f4340d7343d3a1c2911d33045e1de4863f
AtifAziz600 <feralligatrfahim630@gmail.com>
2025-06-14 23:29:38 +0600

    u

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,25 +1,60 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-
   const currency = import.meta.VITE_CURRENCY;
   const navigate = useNavigate();
-  const [user, setUser] = useState(true); // in here with the conditinal rendering method we can say that user is logged in or not same with the profile image backend
+  const [user, setUser] = useState(true); // in here with the conditional rendering method we can say that user is logged in or not same with the profile image backend
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
 
   const fetchProducts = async () => {
     setProducts([]);
-  }
+  };
+
+  // Add Product to Cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+
+    setCartItems(cartData);
+    toast.success("Added to Cart");
+  };
+
+  // Update Product Cart
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart Updated");
+  };
+
+  // Remove Product Cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Removed from cart");
+    setCartItems(cartData);
+  };
 
   useEffect(() => {
     fetchProducts();
-  }, [])
+  }, []);
 
   const value = {
     navigate,
@@ -30,7 +65,11 @@ export const AppContextProvider = ({ children }) => {
     showUserLogin,
     setShowUserLogin,
     products,
-    currency
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
