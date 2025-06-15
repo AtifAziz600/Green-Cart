1ca07f819c5f5ad14c018c631bc2d582f6bfb9ed
AtifAziz600 <feralligatrfahim630@gmail.com>
2025-06-15 19:29:47 +0600

    Recovered project - fresh Git init

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
+  const navigate = useNavigate();
+  const [user, setUser] = useState(true); // in here with the conditional rendering method we can say that user is logged in or not same with the profile image backend
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
+
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  // Add Product to Cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+
+    setCartItems(cartData);
+    toast.success("Added to Cart");
+  };
+
+  // Update Product Cart
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart Updated");
+  };
+
+  // Remove Product Cart
+  const removeFromCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Removed from cart");
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromCart,
+    cartItems,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
